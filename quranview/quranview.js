d574b837c43b9ed5f94f5f721d9e0a860a8ab542
SunniPedia <sunnipedia0@gmail.com>
2025-05-28 16:42:52 +0600

    Update quranview.js

--- quranview/quranview.js
+++ quranview/quranview.js
@@ -224,7 +224,7 @@ function playPrevious() {
 
 document.addEventListener("DOMContentLoaded", () => {
   audioElements = Array.from(document.querySelectorAll('img[alt="Play"]'));
-
+document.getElementById('mainPlayPause').src = 'play.png';
   // ১০ সেকেন্ড পরে অডিও কন্ট্রোলার দৃশ্যমান করো
   setTimeout(() => {
     const controller = document.getElementById('audioController');
